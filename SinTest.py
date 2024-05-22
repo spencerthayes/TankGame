e731f73fec06cada3bd43fb97decfb816f5f46a6
spencerthayes <spencerhayes@me.com>
2024-05-22 17:05:40 -0400

    added sprites and made the angle system easier to work with

--- SinTest.py
+++ SinTest.py
@@ -12,6 +12,11 @@ red = pygame.Color(255,0,0)
 clock = pygame.time.Clock()
 FPS = 60
 
+upSprite = pygame.image.load('Up.png')
+downSprite = pygame.image.load('Down.png')
+rightSprite = pygame.image.load('Right.png')
+leftSprite = pygame.image.load('Left.png')
+
 window = pygame.display.set_mode((displayWidth, displayHeight))
 
 def sin(angle):
@@ -30,8 +35,29 @@ class player(object):
 		self.turningCClockwise = False
 	
 	def draw(self, win):
-		pygame.draw.circle(win, black, (self.x, self.y), 10)
+#		pygame.draw.circle(win, black, (self.x, self.y), 10)
 		pygame.draw.line(win, green, (self.x, self.y), (self.x + 20 * cos(self.angle), self.y + 20 * sin(self.angle)))
+		if self.angle > 315 or self.angle <= 45:
+			spriteRect = rightSprite.get_rect()
+			spriteRect.center = self.x, self.y
+			window.blit(rightSprite, spriteRect)
+					
+		elif self.angle > 45 and self.angle <= 135:
+			spriteRect = downSprite.get_rect()
+			spriteRect.center = self.x, self.y
+			window.blit(downSprite, spriteRect)
+					
+		elif self.angle > 135 and self.angle <= 225:
+			spriteRect = leftSprite.get_rect()
+			spriteRect.center = self.x, self.y
+			window.blit(leftSprite, spriteRect)
+					
+		elif self.angle > 225 and self.angle <= 315:
+			spriteRect = upSprite.get_rect()
+			spriteRect.center = self.x, self.y
+			window.blit(upSprite, spriteRect)
+			
+		
 		
 class projectile(object):
 	def __init__(self, x, y, angle, velocity, colour):
@@ -75,6 +101,9 @@ while not gameExit:
 			elif event.key == pygame.K_z:
 				bulletAngle = tank.angle
 				bullets.append(projectile(tank.x, tank.y, tank.angle, 2, red))
+			elif event.key == pygame.K_LSHIFT:	
+				tank.x += 30 * cos(tank.angle)
+				tank.y += 30 * sin(tank.angle)
 		elif event.type == pygame.KEYUP:
 			if event.key == pygame.K_LEFT:
 				tank.turningCClockwise = False
@@ -89,9 +118,13 @@ while not gameExit:
 		
 	if tank.turningClockwise:
 		tank.angle += 2.5
+		if tank.angle == 360:
+			tank.angle = 0
 		
 	if tank.turningCClockwise:
 		tank.angle -= 2.5
+		if tank.angle < 0:
+			tank.angle += 360
 		
 	for bullet in bullets:
 		if bullet.x >= 0 and bullet.x <= displayWidth and bullet.y >= 0 and bullet.y <= displayHeight:
